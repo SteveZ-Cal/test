baba4d84a65db8c28c049c65af3bc80fd33c0b3e
SteveZ-Cal <steve.zang007@gmail.com>
2022-05-05 01:12:11 -0700

    water

--- hungry.py
+++ hungry.py
@@ -2,5 +2,8 @@ hungry=input("Are you hungry?")
 if hungry=="yes"
     print("eat")
 else
+    thirsty=input("Are you thirsty?")
+    if thirsty=="yes"
+        print("drink water")
     print("do your homework")
 
